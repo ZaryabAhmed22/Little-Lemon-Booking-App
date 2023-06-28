af5f549909722139b59c174a6607608341497ed0
ZaryabAhmed22 <79729046+ZaryabAhmed22@users.noreply.github.com>
2023-06-28 14:26:31 +0500

    Project completed

--- myapp/models.py
+++ myapp/models.py
@@ -7,5 +7,14 @@ class Booking(models.Model):
     reservation_date = models.DateField()
     reservation_slot = models.SmallIntegerField(default=10)
 
-    def __str__(self): 
+    def __str__(self):
         return self.first_name
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.title
